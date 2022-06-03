b982e800a448e56a4248ed708457f6021773dc66
Onur Tirtir <onurcantirtir@gmail.com>
2022-06-03 14:27:22 +0300

    wip tets

--- src/backend/distributed/executor/insert_select_executor.c
+++ src/backend/distributed/executor/insert_select_executor.c
@@ -304,10 +304,16 @@ NonPushableInsertSelectExecScan(CustomScanState *node)
  * If the INSERT...SELECT has CTEs then these are added to the resulting SELECT instead.
  */
 Query *
-BuildSelectForInsertSelect(Query *insertSelectQuery, bool wrapIfContainsGroupBy)
+BuildSelectForInsertSelect(Query *insertSelectQuery, bool wrapIfContainsGroupBy,
+                           bool *wrapped)
 {
 	RangeTblEntry *selectRte = ExtractSelectRangeTableEntry(insertSelectQuery);
 	Query *selectQuery = selectRte->subquery;
+
+    if (wrapped)
+    {
+        *wrapped = true;
+    }
 
 	/*
 	 * Wrap the SELECT as a subquery if the INSERT...SELECT has CTEs or the SELECT
@@ -330,11 +336,20 @@ BuildSelectForInsertSelect(Query *insertSelectQuery, bool wrapIfContainsGroupBy)
 	{
 		/* top-level set operations confuse the ReorderInsertSelectTargetLists logic */
 		selectQuery = WrapSubquery(selectRte->subquery);
+
+        selectQuery->groupClause = copyObject(insertSelectQuery->groupClause);
 	}
 	else if (selectQuery->groupClause != NULL && wrapIfContainsGroupBy)
 	{
 		selectQuery = WrapSubquery(selectRte->subquery);
 	}
+    else
+    {
+        if (wrapped)
+        {
+            *wrapped = false;
+        }
+    }
 
 	return selectQuery;
 }
